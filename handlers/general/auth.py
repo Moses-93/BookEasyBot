54abd33b5edbe4e680ec30a39e25906373c5f2f9
Moses-93 <moieseenko2001@gmail.com>
2025-02-12 17:24:12 +0200

    move registration to common handlers. change the approach to registration

---
+++ handlers/general/auth.py
@@ -0,0 +1,115 @@
+import logging
+
+from aiogram import Router, F
+from aiogram.types import Message
+from aiogram.filters import CommandStart, BaseFilter
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.fsm.context import FSMContext
+
+from keyboards.admin import admin_keyboard
+from keyboards.general import dynamic_keyboard
+from keyboards.factory_method import IdentifyRole
+from services.api_client import api_client
+from states.auth import AuthState
+from utils import validation as val
+
+
+logger = logging.getLogger(__name__)
+
+router = Router()
+
+MESSAGE = {
+    "info": "Привіт! Цей бот - перевірене рішення для індивідуальних майстрів та салонів Beauty сфери, яке призначене для заощадження часу, ведення обліку та спрощення комунікації з клієнтами",
+    "master": "Привіт {name}! Радий знову тебе бачити.\nПочнімо працювати?!",
+    "user": "Радий вас знову бачити, {name}!\nЧим можу бути корисним?",
+    "sign_up": "Чудовий вибір, ви на шляху до успіху!\nДля реєстрації нам всього лиш потрібен ваш номер телефону. Не хвилюйтеся, ми відповідаємо всім вимогам законодавства, тому використовуємо кодування особистої інформації!",
+    "successful_register_master": "{name}, це успіх! Ознайомтесь з форматами підписки в розділі налаштувань та поспішайте працювати.\nУспіхів Вам!",
+    "successful_register_user": "Чудово! {name}, ви успішно пройшли реєстрацію, та вже можете записатись до вашого майстра!",
+}
+
+
+class DeepLinkFilter(BaseFilter):
+    def __init__(self, prefix: str):
+        self.prefix = prefix
+
+    async def __call__(self, message: Message) -> bool:
+        if not message.text.startswith("/start "):
+            return False
+        deep_link_param = (
+            message.text.split(" ", 1)[1] if len(message.text.split()) > 1 else None
+        )
+        return deep_link_param and deep_link_param.startswith(self.prefix)
+
+
+async def process_sign_up(
+    message: Message, state: FSMContext, role: str, user_id: int, master_id: int = None
+):
+    """Обробляє реєстрацію для користувача або майстра."""
+    await state.update_data(
+        name=message.from_user.full_name,
+        username=message.from_user.username,
+        role=role,
+        master_id=master_id,
+        chat_id=user_id,
+    )
+
+    contact_button = KeyboardButton(text="📞 Поділитися номером", request_contact=True)
+    keyboard = ReplyKeyboardMarkup(keyboard=[[contact_button]], resize_keyboard=True)
+
+    await message.answer(
+        text=MESSAGE["sign_up"],
+        reply_markup=keyboard,
+    )
+    await state.set_state(AuthState.phone_number)
+
+
+@router.message(CommandStart())
+async def start(message: Message, user_id: int):
+    status, user = await api_client.get(endpoint="/users/", chat_id=user_id)
+    if status != 200:
+        await message.answer(
+            text=MESSAGE["info"],
+            reply_markup=dynamic_keyboard.dynamic_reply_keyboard(["✅ Стати майстром"]),
+        )
+        return
+    await message.answer(
+        text=MESSAGE[user["role"]].format(name=user["name"]),
+        reply_markup=IdentifyRole.generate_keyboard(user["role"]),
+    )
+
+
+router.message(F.text == "✅ Стати майстром")
+
+
+async def to_be_master(message: Message, state: FSMContext, user_id: int):
+    await state.set_state(AuthState.phone_number)
+
+    await process_sign_up(message, state, "master", user_id)
+
+
+@router.message(AuthState.phone_number)
+async def finish_sign_up(message: Message, state: FSMContext, user_id: int):
+    phone_number = message.contact.phone_number
+    logger.info(f"Phone_number:{phone_number}")
+
+    await state.update_data(phone=phone_number)
+    data = await state.get_data()
+    status, user = await api_client.post(f"/users/", chat_id=user_id, json=data)
+    if status == 201:
+        await message.answer(
+            text=MESSAGE[f"successful_register_{user["role"]}"].format(
+                name=user["name"]
+            ),
+            reply_markup=admin_keyboard.admin_main_menu(),
+        )
+
+
+@router.message(CommandStart(deep_link=True), DeepLinkFilter(prefix="sign-up=master_"))
+async def start_sign_up(message: Message, state: FSMContext, user_id: int):
+    deep_link_param = message.text.split(" ", 1)[1]
+    try:
+        master_id = int(deep_link_param.split("_")[1])
+    except (IndexError, ValueError):
+        await message.answer("Посилання має некоректний формат!")
+        return
+    await process_sign_up(message, state, "user", user_id, master_id)
