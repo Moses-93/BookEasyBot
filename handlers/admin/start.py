3b32e2eb1ea3ac75b48267799cac6393b862ecd7
Moses-93 <moieseenko2001@gmail.com>
2025-02-23 18:15:15 +0200

    add request user to the handler

--- handlers/admin/start.py
+++ handlers/admin/start.py
@@ -3,7 +3,9 @@ import logging
 from aiogram import Router, F
 from aiogram.types import Message
 
-from keyboards.admin import reply_keyboard
+from keyboards.admin import admin_keyboard
+
+from services.api_client import api_client
 
 
 logger = logging.getLogger(__name__)
@@ -12,8 +14,10 @@ router = Router()
 
 
 @router.message(F.text == "admin")
-async def start(message: Message):
+async def start(message: Message, user_id: int):
+    await api_client.get(endpoint="/users/", chat_id=user_id)
+
     await message.answer(
         text="Ви перейшли в адмін панель",
-        reply_markup=reply_keyboard.admin_main_menu(),
+        reply_markup=admin_keyboard.admin_main_menu(),
     )
