30d7cda80c821df42a88a46a8daf753a71adafff
Moses-93 <moieseenko2001@gmail.com>
2025-03-09 16:57:22 +0200

    Update middleware. Add new exception.

--- core/middleware.py
+++ core/middleware.py
@@ -2,8 +2,12 @@ import logging
 from aiogram import BaseMiddleware
 from aiohttp import ClientResponseError
 from aiogram.types import Message, CallbackQuery
-from core.constants.error_message import GENERAL_ERROR_MESSAGES, SCHEDULE_ERROR_MESSAGES
-from core.exceptions.schedule_exception import ScheduleServiceHTTPError
+from core.constants.error_message import (
+    GENERAL_ERROR_MESSAGES,
+    SCHEDULE_ERROR_MESSAGES,
+    SERVICE_ERROR_MESSAGE,
+)
+from core.exceptions import schedule_exception, service_exception
 
 
 logger = logging.getLogger(__name__)
@@ -27,15 +31,26 @@ class ErrorHandlingMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: Message | CallbackQuery, data: dict):
         try:
             return await handler(event, data)
-        except ScheduleServiceHTTPError as e:
+        except schedule_exception.ScheduleServiceHTTPError as e:
             await self.handle_schedule_error(event, e)
         except ClientResponseError as e:
             await self.handle_client_error(event, e)
+        except service_exception.ServiceHTTPError as e:
+            await self.handle_service_error(event, e)
         # except Exception as e:
         #     await self.handle_general_error(event, e)
 
+    async def handle_service_error(
+        self, event: Message | CallbackQuery, error: service_exception.ServiceHTTPError
+    ):
+        logger.info(f"Error: {error}")
+        message = SERVICE_ERROR_MESSAGE.get(error.status)
+        await send_response(event, message)
+
     async def handle_schedule_error(
-        self, event: Message | CallbackQuery, error: ScheduleServiceHTTPError
+        self,
+        event: Message | CallbackQuery,
+        error: schedule_exception.ScheduleServiceHTTPError,
     ):
         logger.info(f"Error: {error}")
         message = SCHEDULE_ERROR_MESSAGES.get(error.status)
