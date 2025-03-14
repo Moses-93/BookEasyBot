e7242146ac040cb68b8e5b1d7ab719d2a0e4855c
Moses-93 <moieseenko2001@gmail.com>
2025-03-14 12:47:15 +0200

    Update the subscriptions module. Add and register new handlers

--- handlers/admin/subscriptions/subscription_handlers.py
+++ handlers/admin/subscriptions/subscription_handlers.py
@@ -1,2 +1,40 @@
+import logging
+from aiogram.types import Message, CallbackQuery
+from services.subscriptions.subscription_manager import SubscriptionManager
+
+
+logger = logging.getLogger(__name__)
+
+
 class SubscriptionCommandHandler:
-    pass
+
+    def __init__(self, subscription_manager: SubscriptionManager):
+        self.subscription_manager = subscription_manager
+
+    async def start(self, message: Message, user_id: int):
+        msg, keyboard = await self.subscription_manager.start(user_id)
+        await message.answer(text=msg, reply_markup=keyboard)
+
+    async def buy_subscription(self, message: Message, user_id: int):
+        msg, keyboard = await self.subscription_manager.create_payment_link(user_id)
+        await message.answer(text=msg, reply_markup=keyboard, parse_mode="Markdown")
+
+    async def cancel_user_subscription(self, message: Message, user_id: int):
+        msg, keyboard = await self.subscription_manager.cancel_user_subscription(
+            user_id
+        )
+        await message.answer(text=msg, reply_markup=keyboard, parse_mode="Markdown")
+
+    async def update_user_subscription(self, message: Message, user_id: int):
+        msg, keyboard = await self.subscription_manager.update_user_subscription(
+            user_id
+        )
+        await message.answer(text=msg, reply_markup=keyboard, parse_mode="Markdown")
+
+    async def active_free_subscription(self, message: Message, user_id: int):
+        msg = await self.subscription_manager.active_free_subscription(user_id)
+        await message.answer(text=msg)
+
+    async def show_user_subscription(self, message: Message, user_id: int):
+        msg = await self.subscription_manager.show_user_subscription(user_id)
+        await message.answer(text=msg, parse_mode="Markdown")
--- handlers/admin/subscriptions/subscription_router.py
+++ handlers/admin/subscriptions/subscription_router.py
@@ -10,4 +10,16 @@ class SubscriptionRouter:
 
     def _register_handlers(self, handler: SubscriptionCommandHandler):
         """Реєстрація обробників."""
-        pass
+        self.router.message.register(handler.start, F.text == "💳 Підписка")
+        self.router.message.register(
+            handler.buy_subscription, F.text == "💳 Придбати підписку"
+        )
+        self.router.message.register(
+            handler.active_free_subscription, F.text == "🆓 Спробувати безкоштовно"
+        )
+        self.router.message.register(
+            handler.show_user_subscription, F.text == "📜 Моя підписка"
+        )
+        self.router.message.register(
+            handler.cancel_user_subscription, F.text == "❌ Скасувати підписку"
+        )
