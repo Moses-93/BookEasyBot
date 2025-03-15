0a6d75f911e0f35d77f77c36e04c6a6d08ae3664
Moses-93 <moieseenko2001@gmail.com>
2025-03-15 11:39:09 +0200

    Update navigator

--- services/navigation.py
+++ services/navigation.py
@@ -15,20 +15,22 @@ class NavigationManager:
     async def go_to(self, user_id: int, state: FSMContext, new_section: str):
         """Moves to a new section, adding it to the history"""
         await state.clear()
+
+        self.history.setdefault(user_id, ["main_menu"]).append(new_section)
+
         logger.info(
             f"User navigated to {new_section}. History: {self.history[user_id]}"
         )
-        self.history.setdefault(user_id, ["main_menu"]).append(new_section)
         logger.info(f"History after: {self.history}")
 
     async def go_back(self, user_id: int, state: FSMContext):
         """Returns the user to the previous section"""
         await state.clear()
 
-        logger.info(f"User navigation history: {self.history[user_id]}")
-
         if user_id not in self.history or len(self.history[user_id]) <= 1:
             return self.keyboard.main_menu()
+
+        logger.info(f"User navigation history: {self.history[user_id]}")
 
         self.history[user_id].pop()
         preview_section = self.history[user_id][-1]
